a82eee35edc6652055627308ba6c317ffbaeaf6e
Momna <momnaazmat786@gmail.com>
2021-10-23 14:32:50 -0700

    final commit

--- components/Map.js
+++ components/Map.js
@@ -1,13 +1,22 @@
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import { useState } from "react";
+import ReactMapGL, {Marker, Popup} from "react-map-gl";
 import getCenter from "geolib/es/getCenter";
 
-function Map(searchResults) {
+function Map({ searchResults }) {
+    const [selectedLocation, setSelectedLocation] = useState({});
+
+    const coordinates = searchResults.map((result) => ({
+        longitude: result.long,
+        latitude: result.lat,
+    }));
+
+    const center = getCenter(coordinates);
+
     const [viewport, setViewport] = useState({
         width: "100%",
         height: "100%",
-        latitude: 37.7577,
-        longitude: -122.4376,
+        latitude: center.latitude,
+        longitude: center.longitude,
         zoom: 11,
     });
     
@@ -17,7 +26,36 @@ function Map(searchResults) {
             mapboxApiAccessToken={process.env.mapbox_key}
             {...viewport}
             onViewportChange={(nextViewport) => setViewport(nextViewport) }
-        ></ReactMapGL>
+        >
+            {searchResults.map((result) => (
+                <div key={result.long}>
+                    <Marker
+                        longitude={result.long}
+                        latitude={result.lat}
+                        offsetLeft={-20}
+                        offsetTop={-10}
+                    >
+                        <p role="img" onClick={() => setSelectedLocation(result)} className="cursor-pointer text-xl animate-bounce" aria-label="push-pin">
+                        📌
+                        </p>
+                    </Marker>
+
+                    {selectedLocation.long === result.long ? (
+                        <Popup
+                            onClose={() => setSelectedLocation({})}
+                            closeOnClick={true}
+                            latitude={result.lat}
+                            longitude={result.long}
+                        >
+                            {result.title}
+                        </Popup>
+                    ):(
+                        false
+                    )}
+
+                </div>
+            ))}
+        </ReactMapGL>
     );
 }
 
